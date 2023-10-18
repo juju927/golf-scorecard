f8738ede5c716b1b6ef19c533c4b9a5323cdf643
927joanna <sot.joanna@gmail.com>
2023-10-18 16:12:09 +0800

    fix: createRound now initialises with an array for round_record

--- gsc-backend/controllers/userRecordsCtrl.js
+++ gsc-backend/controllers/userRecordsCtrl.js
@@ -7,14 +7,14 @@ const sendResponse = require("../helpers/sendResponseHelper");
 
 async function createRound(req, res) {
   try {
-    const user = await User.findById(req.body.user_id) // change to token (req.user._id)
+    const user = await User.findById(req.body.user_id) //! change to token (req.user._id)
     const course = await Course.findById(req.body.course_id)
     const newRound = await UserRecord.create({
       user_id: user._id, // change to token (req.user._id)
       course_id: course._id,
       round_date: req.body.round_date || Date.now(), 
       tee: req.body.tee, // do a retrieve of tees in the course
-      round_record: []
+      round_record: [...initialiseRecord(req.body.round_type)]
     })
     sendResponse(res, 201, { newRound }, `round started at ${newRound.round_date}`)
   } catch (err) {
@@ -22,5 +22,35 @@ async function createRound(req, res) {
     sendResponse(res, 500, err.message);
   }
 }
+
+// async function addStroke(req, res) {
+//   const round = await UserRecord.findById(req.body.round_id)
+//   round.round_
+// }
+
+function initialiseRecord(roundType) {
+  // roundLength is 'full', 'out', or 'in'
+  let start = 1
+  let end = 18
+  if (roundType == 'out') {
+    end = 9
+  } else if (roundType == 'in') {
+    start = 10
+  }
+
+  const arr = []
+
+  for (let i=start; i<=end; i++) {
+    arr.push({
+      hole_num: i,
+      total_strokes: 0,
+      penalty_strokes: 0,
+      stroke_details: []
+    })
+  }
+
+  return arr
+}
+
 
 module.exports = { createRound };
