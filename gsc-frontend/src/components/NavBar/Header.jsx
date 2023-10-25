b5935bf8003821c8a23ec1ea83d40a87755f0c9f
927joanna <sot.joanna@gmail.com>
2023-10-25 12:00:05 +0800

    feat: create home page w/ 1 link to start record

--- gsc-frontend/src/components/NavBar/Header.jsx
+++ gsc-frontend/src/components/NavBar/Header.jsx
@@ -3,9 +3,9 @@ import logo from "../../assets/images/golf-buddy-logo.png";
 const NavBar = () => {
   return (
     <>
-      <header className="h-16 w-screen flex justify-center bg-teal-950 slate-950 border border-b-teal-600 px-2">
+      <div className="h-16 w-screen flex justify-center bg-teal-950 slate-950 border border-b-teal-600 px-2">
         <img className="p-2" src={logo}></img>
-      </header>
+      </div>
     </>
   );
 };
--- gsc-frontend/src/pages/HomePage.jsx
+++ gsc-frontend/src/pages/HomePage.jsx
@@ -1,9 +1,31 @@
-
+import Header from "../components/NavBar/Header";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900">
-      hoem pej :)
+      <Header />
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="text-center sm:text-left">
+            <h1 className="text-2xl font-bold text-white sm:text-3xl">
+              Welcome Back, name!
+            </h1>
+
+            <p className="mt-1.5 text-sm text-gray-500">
+              Let's hit some good shots today! 🎉
+            </p>
+          </div>
+
+          <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+            <button
+              className="block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
+              type="button"
+            >
+              Record New Game
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
