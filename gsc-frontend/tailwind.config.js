c064d26422e83b271c08953ae59488383099afa2
927joanna <sot.joanna@gmail.com>
2023-12-15 17:52:54 +0800

    Revert "fix: vh spill #3"
    This reverts commit 269ba9c11c69a95b20f437b0b63c6b7da3ef67d4.

--- gsc-frontend/tailwind.config.js
+++ gsc-frontend/tailwind.config.js
@@ -5,5 +5,17 @@ export default {
   theme: {
     extend: {},
   },
+  methods: {
+    setViewHeight: function () {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    },
+  },
+  mounted: function () {
+    this.setViewHeight();
+    window.addEventListener("resize", () => {
+      this.setViewHeight();
+    });
+  },
   plugins: [forms],
 };
