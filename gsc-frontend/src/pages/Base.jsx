52229a523831055149dd61383f1d61a5776c6501
927joanna <sot.joanna@gmail.com>
2023-12-15 15:39:11 +0800

    Revert "fix: mobile friendly vh"
    This reverts commit 5d5fb9878fb60ed909ad3c05b338111eb77eb36f.

--- gsc-frontend/src/pages/Base.jsx
+++ gsc-frontend/src/pages/Base.jsx
@@ -1,14 +1,14 @@
-import { Outlet } from "react-router-dom";
-import { getUser } from "../utilities/users-service";
-import { Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom"
+import { getUser } from "../utilities/users-service"
+import { Navigate } from "react-router-dom"
 
 const Base = () => {
   return (
-    <div className="absolute inset-0">
-      {getUser() ? <Navigate to="/home" /> : <Navigate to="/login" />}
-      <Outlet />
-    </div>
-  );
-};
+    <>
+    { getUser() ? <Navigate to="/home" /> : <Navigate to="/login" />}
+    <Outlet />
+    </>
+  )
+}
 
-export default Base;
+export default Base
