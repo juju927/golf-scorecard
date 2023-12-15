d57925be468ec43d2fb399207f0fe1a41eaa6a68
927joanna <sot.joanna@gmail.com>
2023-12-15 12:33:00 +0800

    feat: improve visibility of loader

--- gsc-frontend/src/components/common/Loading.jsx
+++ gsc-frontend/src/components/common/Loading.jsx
@@ -2,7 +2,11 @@ import { metronome } from "ldrs";
 
 const Loading = () => {
   metronome.register();
-  return <l-metronome size="40" speed="1.6" color="black"></l-metronome>;
+  return (
+    <div className="w-full flex justify-center p-6">
+      <l-metronome size="40" speed="1.6" color="white"></l-metronome>
+    </div>
+  );
 };
 
 export default Loading;
