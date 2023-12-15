5d5fb9878fb60ed909ad3c05b338111eb77eb36f
927joanna <sot.joanna@gmail.com>
2023-12-15 15:30:40 +0800

    fix: mobile friendly vh

--- gsc-frontend/src/pages/Base.jsx
+++ gsc-frontend/src/pages/Base.jsx
@@ -1,14 +1,14 @@
-import { Outlet } from "react-router-dom"
-import { getUser } from "../utilities/users-service"
-import { Navigate } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import { getUser } from "../utilities/users-service";
+import { Navigate } from "react-router-dom";
 
 const Base = () => {
   return (
-    <>
-    { getUser() ? <Navigate to="/home" /> : <Navigate to="/login" />}
-    <Outlet />
-    </>
-  )
-}
+    <div className="absolute inset-0">
+      {getUser() ? <Navigate to="/home" /> : <Navigate to="/login" />}
+      <Outlet />
+    </div>
+  );
+};
 
-export default Base
+export default Base;
