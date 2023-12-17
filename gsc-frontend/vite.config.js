3a14d8f9c134ace43330bd1c3643e68305709fbb
927joanna <sot.joanna@gmail.com>
2023-12-17 15:16:58 +0800

    feat: implement vite pwa

--- gsc-frontend/vite.config.js
+++ gsc-frontend/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      "/api": {
+        target: "http://localhost:3001",
         changeOrigin: true,
-        secure: false
-      }
-    }
+        secure: false,
+      },
+    },
   },
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+});
