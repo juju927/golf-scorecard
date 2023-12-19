9d36a726d8d3b19c4a73e1a1682f4d1c6500661f
927joanna <sot.joanna@gmail.com>
2023-12-19 20:10:37 +0800

    fix: table tallies now show score instead of stroke total

--- gsc-frontend/src/components/scorecard/TableView/TableIORow.jsx
+++ gsc-frontend/src/components/scorecard/TableView/TableIORow.jsx
@@ -1,11 +1,15 @@
 const TableIORow = ({ io, rowValues }) => {
   return (
     <div className="h-10 flex text-white justify-between text-center items-center lining-nums border-t-2 border-white text-lg font-semibold">
-        <div className="w-1/6 uppercase">{io}</div>
-        <div className="w-1/5">{rowValues?.dist}</div>
-        <div className="w-1/5"></div>
-        <div className="w-1/5">{rowValues?.par}</div>
-        <div className="w-1/6">{rowValues?.strokes}</div>
+      <div className="w-1/6 uppercase">{io}</div>
+      <div className="w-1/5">{rowValues?.dist}</div>
+      <div className="w-1/5"></div>
+      <div className="w-1/5">{rowValues?.par}</div>
+      {/* <div className="w-1/6">{rowValues?.strokes}</div> */}
+      <div className="w-1/6">
+        {rowValues?.score > 0 && "+"}
+        {rowValues?.score}
+      </div>
     </div>
   );
 };
--- gsc-frontend/src/components/scorecard/TableView/TableTotalRow.jsx
+++ gsc-frontend/src/components/scorecard/TableView/TableTotalRow.jsx
@@ -5,7 +5,11 @@ const TableTotalRow = ({ rowValues }) => {
       <div className="w-1/5">{rowValues?.dist}</div>
       <div className="w-1/5"></div>
       <div className="w-1/5">{rowValues?.par}</div>
-      <div className="w-1/6">{rowValues?.strokes}</div>
+      {/* <div className="w-1/6">{rowValues?.strokes}</div> */}
+      <div className="w-1/6">
+        {rowValues?.score > 0 && "+"}
+        {rowValues?.score}
+      </div>
     </div>
   );
 };
