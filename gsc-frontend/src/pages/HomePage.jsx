a2c95005e1d1183174015998f7b552b821392be9
927joanna <sot.joanna@gmail.com>
2023-12-31 13:28:16 +0800

    fix: add logout button

--- gsc-frontend/src/pages/HomePage.jsx
+++ gsc-frontend/src/pages/HomePage.jsx
@@ -14,11 +14,14 @@ import golfBag from "../assets/images/golf-bag.png";
 import dayjs from "dayjs";
 import RoundListItem from "../components/record/ListItems/RoundListItem";
 import newGame from "../assets/images/newGame.png";
-import editGolfBag from "../assets/images/editGolfBag.png"
-import analyseScorecard from "../assets/images/analyseScorecard.png"
+import editGolfBag from "../assets/images/editGolfBag.png";
+import analyseScorecard from "../assets/images/analyseScorecard.png";
+import logOut from "../assets/images/logOut.png";
+import { logOutService } from "../utilities/users-service";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  const user = useAtomValue(userAtom);
+  const [user, setUser] = useAtom(userAtom);
   const userProfile = useAtomValue(userProfileAtom);
   const [userRounds, setUserRounds] = useAtom(userRoundsAtom);
   const setCurrentRound = useSetAtom(currentRoundRecordAtom);
@@ -26,39 +29,49 @@ const HomePage = () => {
   const [todayRounds, setTodayRounds] = useState([]);
   const quickActions = [
     {
-    label: "new game",
-    img: newGame,
-    link: "/record/new"
-  },
+      label: "new game",
+      img: newGame,
+      link: "/record/new",
+    },
     {
-    label: "review",
-    img: analyseScorecard,
-    link: "/analyse"
-  },
-  {
-    label: "edit profile",
-    img: editGolfBag,
-    link: "/profile"
-  },
-  //   {
-  //   label: "new game",
-  //   img: newGame,
-  //   link: "/record/new"
-  // },
-]
+      label: "review",
+      img: analyseScorecard,
+      link: "/analyse",
+    },
+    {
+      label: "edit profile",
+      img: editGolfBag,
+      link: "/profile",
+    },
+    //   {
+    //   label: "log out",
+    //   img: logOut,
+    //   link: "/logout"
+    // },
+  ];
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    logOutService();
+    setUser(null);
+    navigate("/login");
+    // setTimeout(() => {
+    //   navigate("/login");
+    // }, 2000);
+  };
 
   useEffect(() => {
     setUserRounds(loaderUserRounds);
   }, [loaderUserRounds]);
 
-  useEffect(()=> {
+  useEffect(() => {
     const filteredRounds = userRounds.filter(
       (round) =>
         dayjs(round?.date).format("D MMM YYYY") ==
         dayjs(Date.now()).format("D MMM YYYY")
     );
     setTodayRounds(filteredRounds);
-  }, [userRounds])
+  }, [userRounds]);
 
   return (
     <div className="w-screen h-screen flex flex-col">
@@ -112,21 +125,34 @@ const HomePage = () => {
           ))}
 
           <div className="grid grid-cols-4 pt-8">
-            { quickActions.map((quickAction, idx)=> (
-              <div key={idx} className="w-full h-fit flex flex-col items-center justify-center">
-              <Link
-                to={quickAction.link}
-                className="block w-fit rounded-lg bg-teal-500 p-3"
-                type="button"
+            {quickActions.map((quickAction, idx) => (
+              <div
+                key={idx}
+                className="w-full h-fit flex flex-col items-center justify-center"
               >
-                <img src={quickAction.img} className="w-10" />
-              </Link>
+                <Link
+                  to={quickAction.link}
+                  className="block w-fit rounded-lg bg-teal-500 p-3"
+                  type="button"
+                >
+                  <img src={quickAction.img} className="w-10" />
+                </Link>
+                <span className="text-center text-xs font-medium text-white">
+                  {quickAction.label}
+                </span>
+              </div>
+            ))}
+            <div
+              className="w-full h-fit flex flex-col items-center justify-center"
+              onClick={handleLogout}
+            >
+              <div className="block w-fit rounded-lg bg-teal-500 p-3">
+                <img src={logOut} className="w-10" />
+              </div>
               <span className="text-center text-xs font-medium text-white">
-                {quickAction.label}
+                log out
               </span>
             </div>
-            ))}
-
           </div>
         </div>
       </div>
